0903e9f51dbe92995cfe1a00baa231d56821d645
hungngocphat01 <hungngocphat01@gmail.com>
2021-05-21 23:00:12 -0700

    Thêm class để quản lý menu

---
+++ 19120615-HeroFighter/Menu.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include<string>
+#include<vector>
+using namespace std;
+
+// Kiểu con trỏ trỏ tới 1 hàm void
+typedef void (*vfuncptr_t)();
+
+class Menu
+{
+private:
+	vector<string> entries;
+	vector<vfuncptr_t> actions;
+public:
+	void AddNewEntry(string, vfuncptr_t);
+	void Show();
+};
+
