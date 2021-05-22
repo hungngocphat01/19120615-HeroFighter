bb1251fe6e8dad8dfda16307abdbf09f478a0726
hungngocphat01 <hungngocphat01@gmail.com>
2021-05-21 23:00:38 -0700

    Hoàn thiện hàm InsertNewMember

--- 19120615-HeroFighter/Team.cpp
+++ 19120615-HeroFighter/Team.cpp
@@ -1,4 +1,14 @@
-#include "Team.h"
+﻿#include "Team.h"
+
+void Team::InsertNewMember(string data)
+{
+	Hero* newhero = Hero::CreateNewHero(data);
+	// Cập nhật lại điểm số cho từng cặp hero mỗi khi thêm vào
+	for (Hero*& member : members) {
+		newhero->UpdtPntsInTeam(*member);
+	}
+	members.push_back(newhero);
+}
 
 void Team::UpdateEnvironment(attribute_t env)
 {
