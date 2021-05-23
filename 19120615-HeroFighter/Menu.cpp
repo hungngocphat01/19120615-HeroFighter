86a1457de7aea4190b6eaeb828a0cd4b96e977c5
hungngocphat01 <hungngocphat01@gmail.com>
2021-05-23 23:58:43 +0700

    Thêm khả năng tuỳ chỉnh cho hàm show

--- 19120615-HeroFighter/Menu.cpp
+++ 19120615-HeroFighter/Menu.cpp
@@ -1,23 +1,34 @@
 #include "Menu.h"
 
+// Lambda expression
+void printLine()
+{
+	cout << "--------------------------" << endl;
+};
+
+Menu::Menu(string header, string title)
+{
+	this->header = header;
+	this->menu_title = title;
+}
+
+Menu::Menu(string title)
+{
+	this->menu_title = title;
+}
+
 void Menu::themEntryMoi(string description, vfuncptr_t callback)
 {
 	this->arrEntries.push_back(description);
 	this->arrActions.push_back(callback);
 }
 
-void Menu::hienThi(bool clrscr)
+void Menu::hienThi(vfuncptr_t customfunc, bool clrscr)
 {
 	if (clrscr) 
 	{
 		system("cls");
 	}
-
-	// Lambda expression
-	vfuncptr_t printLine = []()
-	{
-		cout << "--------------------------" << endl;
-	};
 
 	while (true)
 	{
@@ -26,8 +37,12 @@ void Menu::hienThi(bool clrscr)
 		cout << menu_title << endl;
 		printLine();
 
+		if (customfunc != nullptr) {
+			customfunc();
+		}
+
 		// In ra các arrEntries
-		for (int i = 0; i < arrEntries.size(); i++) {
+		for (unsigned i = 0; i < arrEntries.size(); i++) {
 			cout << i + 1 << ". " << arrEntries[i] << "." << endl;
 		}
 		cout << "0. Quay lai menu truoc" << endl;
@@ -44,4 +59,13 @@ void Menu::hienThi(bool clrscr)
 		vfuncptr_t action = arrActions[choice];
 		action();
 	}
+}
+
+void Menu::customMenu(vfuncptr_t customfunc)
+{
+	cout << header << endl;
+	printLine();
+	cout << menu_title << endl;
+	printLine();
+	customfunc();
 }
--- 19120615-HeroFighter/Menu.h
+++ 19120615-HeroFighter/Menu.h
@@ -2,14 +2,15 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <functional>
 using namespace std;
 
 /*
 * Đây là class em thiết kế thêm để tự động hoá việc tạo và xử lí các menu
 */
 
-// Kiểu con trỏ trỏ tới 1 hàm void
-typedef void (*vfuncptr_t)();
+// Alias cho delegate kiểu void() -> void
+using vfuncptr_t = function<void()>;
 
 class Menu
 {
@@ -18,8 +19,13 @@ private:
 	vector<string> arrEntries;
 	vector<vfuncptr_t> arrActions;
 public:
-	string header;
+	string header = "HERO FIGHTER SIMULATOR\n(c) 2021 HUNG NGOC PHAT 19120615";
 	string menu_title;
+
+	// Constructor có cả header và title
+	Menu(string, string);
+	// Constructor chỉ có title
+	Menu(string);
 
 	// Hàm thêm 1 entry mới vào menu, gồm tên entry 
 	// và con trỏ hàm trỏ tới code cần thực thi khi chọn entry đó
@@ -27,6 +33,12 @@ public:
 
 	// Hiển thị một menu, lặp mãi mãi đến khi user chọn thoát
 	// Nếu có exception thì throw về menu trước, đến main menu thì throw ra hàm main
-	void hienThi(bool clrscr = true);
+	// Tham số đầu: phần code custom cần chạy trước khi hiển thị menu (mặc định là không)
+	// Tham số sau: có cần clear screen hay không (mặc định là có)
+	void hienThi(vfuncptr_t custom = nullptr, bool clrscr = true);
+
+	// Chạy menu nhưng không hiển thị nội dung menu mà chỉ hiện tiêu đề menu,
+	// nội dung tuỳ chỉnh được truyền vào dưới dạng lambda expression
+	void customMenu(vfuncptr_t);
 };
 
