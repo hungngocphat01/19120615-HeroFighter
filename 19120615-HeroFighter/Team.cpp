1eb71dac6408d0d24bed4d19ab85adfc06160c1c
hungngocphat01 <hungngocphat01@gmail.com>
2021-05-27 10:14:20 +0700

    Implement nhiều hàm

--- 19120615-HeroFighter/Team.cpp
+++ 19120615-HeroFighter/Team.cpp
@@ -10,9 +10,10 @@ Team::Team()
 	strTenDoiChoi = "";
 }
 
-void Team::themThanhVienMoi(string data)
+void Team::themThanhVienMoi(string hero)
 {
-	Hero* newhero = Hero::taoHeroMoi(data);
+	Hero* newhero = Hero::taoHeroMoi(hero);
+
 	// Cập nhật lại điểm số cho từng cặp hero mỗi khi thêm vào
 	for (Hero*& member : arrThanhVien) {
 		newhero->capNhtThgSoTrongTeam(*member);
@@ -25,6 +26,124 @@ void Team::capNhatMoiTruong(attribute_t mtr)
 	for (Hero*& member : arrThanhVien) {
 		member->capNhtThgSoTheoMoiTrg(mtr);
 	}
+}
+
+void Team::hienThiLucLuong()
+{
+	cout << "Luc luong cua " << this->strTenDoiChoi << endl;
+	cout << setw(15) << "Ten"
+		<< setw(4) << "Mau"
+		<< setw(4) << "Cong"
+		<< setw(4) << "Thu"
+		<< setw(4) << "Toc do"
+		<< endl;
+
+	for (Hero* hero : arrThanhVien)
+	{
+		cout << setw(15) << hero->getTen()
+			<< setw(4) << hero->getMau()
+			<< setw(4) << hero->getCong()
+			<< setw(4) << hero->getThu()
+			<< setw(4) << hero->getThGianCho() 
+			<< endl;
+	}
+}
+
+int Team::soLuongThanhVien()
+{
+	return this->arrThanhVien.size();
+}
+
+void Team::luaChon()
+{
+	Menu menu("Lua chon thanh vien cho " + this->strTenDoiChoi);
+	bool valid = true;
+	while (!valid)
+	{
+		menu.inHeader();
+		cout << "Hay nhap 3 con so lien tiep (cach nhau boi dau cach) de chon 3 dong doi cua minh!" << endl;
+		for (int i = 0; i < Hero::danhSachHero.size(); i++)
+		{
+			cout << i + 1 << ". " << Hero::danhSachHero.at(i) << endl;
+		}
+		cout << "Nhap 3 so: ";
+		int x, y, z;
+		cin >> x >> y >> z;
+		cin.ignore();
+
+		if (max({ x, y, z }) > Hero::danhSachHero.size())
+		{
+			cout << "Ban da nhap mot so khong hop le. Vui long kiem tra lai!" << endl;
+			system("pause");
+		}
+	}
+}
+
+void Team::taoNgauNhien()
+{
+	// Những số đã random rồi, không random lại
+	vector<int> daRandomRoi;
+
+	int i = 0;
+	while (i < 3)
+	{
+		int r = rand() % Hero::danhSachHero.size();
+
+		// Nếu chọn rồi thì không chọn lại nữa
+		if (find(daRandomRoi.begin(), daRandomRoi.end(), r) != daRandomRoi.end())
+		{
+			continue;
+		}
+		else
+		{
+			string herodata = Hero::danhSachHero.at(i);
+			Hero* hero = Hero::taoHeroMoi(herodata);
+			this->arrThanhVien.push_back(hero);
+			i++;
+		}
+	}
+}
+
+void Team::xoa()
+{
+	this->strTenDoiChoi = "";
+	this->arrThanhVien.clear();
+}
+
+result_t Team::batDauDauVoi(Team& doithu)
+{
+	// Bắt đầu cho đấu theo từng cặp
+	int thangCount = 0;
+	int thuaCount = 0;
+
+	for (int i = 0; i < 3; i++)
+	{
+		Hero* h1 = this->arrThanhVien[i];
+		Hero* h2 = doithu.arrThanhVien[i];
+		cout << "Cap thu " << i + 1 << ": " << h1->getTen() << " vs " << h2->getTen() << "!" << endl;
+		result_t kq = h1->batDauDanh(*h2);
+
+		switch (kq)
+		{
+		case THANG:
+			cout << h1->getTen() << " cua team 1 da thang!" << endl << endl;
+			thangCount++;
+			break;
+		case THUA:
+			cout << h2->getTen() << " cua team 2 da thang!" << endl << endl;
+			thuaCount++;
+			break;
+		case HUE:
+			cout << "2 ben hoa nhau!" << endl << endl;
+			break;
+		}
+	}
+
+	if (thangCount == thuaCount)
+	{
+		return HUE;
+	}
+	return (thangCount > thuaCount);
 }
 
 Team::~Team()
