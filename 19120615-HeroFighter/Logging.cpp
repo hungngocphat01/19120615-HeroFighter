93f57c5c035775458955620204eaa613828d07a8
hungngocphat01 <hungngocphat01@gmail.com>
2021-05-27 10:12:16 +0700

    Tạo khung sườn

--- 19120615-HeroFighter/Logging.cpp
+++ 19120615-HeroFighter/Logging.cpp
@@ -1,1 +1,19 @@
 #include "Logging.h"
+
+void Logger::themLog(string content)
+{
+	// Implement sau
+	cout << content << endl;
+}
+
+void Logger::docTuFile()
+{
+}
+
+void Logger::ghiXuongFile()
+{
+}
+
+void Logger::ghiNoiTiepXuongFile()
+{
+}
--- 19120615-HeroFighter/Logging.h
+++ 19120615-HeroFighter/Logging.h
@@ -1,12 +1,44 @@
 ﻿#pragma once
-
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
 /*
-* Đây là class em xây dựng thêm để handle logging dễ dàng hơn
+* Đây là module em xây dựng thêm để handle logging dễ dàng hơn
 */
 
-class Logging
-{
-private:
 
+// Một "dòng" trong log, không có gì phức tạp nên em dùng kiểu struct
+struct LogEntry
+{
+	int day;
+	int month;
+	int year;
+	int hour;
+	int minute;
+	int second;
+	string content;
+};
+
+// Class chứa các hàm để quản lí log
+class Logger
+{
+public:
+	// Các "dòng" log
+	static vector<LogEntry> arrEntries;
+	
+	// File log cần sử dụng
+	static ifstream strmLogFile;
+
+	// Thêm một dòng vào log
+	static void themLog(string content);
+	// Đọc log từ file
+	static void docTuFile();
+	// Ghi log xuống file (ghi đè)
+	static void ghiXuongFile();
+	// Ghi nối tiếp log xuống file
+	static void ghiNoiTiepXuongFile();
 };
 
